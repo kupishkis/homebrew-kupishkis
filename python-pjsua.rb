79a855020376cba83176978a629df568fbe03539
Vytis Valentinavičius <vytis.valentinavicius@lamoda.ru>
2014-03-31 16:04:20 +0300

    Now it really really works.

--- python-pjsua.rb
+++ python-pjsua.rb
@@ -1,16 +1,18 @@
 require 'formula'
 
 class PythonPjsua < Formula
-  homepage 'http://www.pjsip.org'
-  head 'http://svn.pjsip.org/repos/pjproject/trunk'
+    version "0.1.0"
+    homepage 'http://www.pjsip.org'
+    head 'http://svn.pjsip.org/repos/pjproject/trunk'
 
-  depends_on :python
+    depends_on :python
 
-  def install
-    ENV.j1
-    system "./configure", "--prefix=#{prefix}"
-    system "make", "dep"
-    system "make"
-    system "cd", "pjsip-apps/src/python", "&&", "python", "setup.py", "install"
-  end
+    def install
+        ENV.j1
+        system "./configure", "--prefix=#{prefix}"
+        system "make", "dep"
+        system "make"
+        cd "pjsip-apps/src/python"
+        system "python", "setup.py", "install"
+    end
 end
