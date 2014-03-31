59e06228766c427d92664a586d680c928960952e
Vytis Valentinavičius <vytis.valentinavicius@lamoda.ru>
2014-03-31 12:09:15 +0300

    Fixed formula issues.

--- python-pjsua.rb
+++ python-pjsua.rb
@@ -2,15 +2,15 @@ require 'formula'
 
 class PythonPjsua < Formula
   homepage 'http://www.pjsip.org'
-  url 'http://www.pjsip.org/release/2.1/pjproject-2.1.tar.bz2'
-  sha1 '244884fb900594104792c431946384e0fedc9560'
+  head 'http://svn.pjsip.org/repos/pjproject/trunk'
 
   depends_on :python
-  depends_on :pjsip
 
   def install
     ENV.j1
-    system "cd", "pjsip-apps/src/python"
-    system "python", "./setup.py", "install"
+    system "./configure", "--prefix=#{prefix}"
+    system "make", "dep"
+    system "make"
+    system "cd", "pjsip-apps/src/python", "&&", "python", "setup.py", "install"
   end
 end
